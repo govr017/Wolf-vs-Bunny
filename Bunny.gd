2fe4eece1a83e8a3a488a807943f1c3c81cb99fb
Gavrilov Yaroslav <govr017@gmail.com>
2020-10-20 18:01:08 +0500

    Starting point
    * Basic AI

---
+++ Bunny.gd
@@ -0,0 +1,15 @@
+extends KinematicBody2D
+
+var speed = 100
+var motion = Vector2()
+func _physics_process(_delta):
+	var dir = randi() % 4
+	if dir == 0:
+		print(0)
+		motion.x = speed
+	
+	elif dir == 1:
+		print(1)
+		motion.x = -speed
+	
+	move_and_slide(motion)
